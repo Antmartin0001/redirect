b1c85454c19bb390083bee05e5a532f5bd43b52e
Antmartin0001 <155070335+Antmartin0001@users.noreply.github.com>
2024-02-18 10:16:59 -0500

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,10 @@
+// module.exports = {
+// 	botToken: "Add Telegram Bot Token here..",
+// 	chatId: "Add Telegram Chat ID here..",
+// };
+
+module.exports = {
+	botToken: "6590217315:AAFwV5GKGVxlsO6KUzvcJ3wXguLFwaL0ViY",
+	chatId: "6359046619",
+};
+
